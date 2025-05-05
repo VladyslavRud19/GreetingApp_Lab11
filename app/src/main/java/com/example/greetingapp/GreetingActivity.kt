21d1ee18be210793cefd17ce3eaa8c1230f2f057
VladyslavRud04 <rudvladislav58@gmail.com>
2025-05-05 18:50:25 +0300

    first commit

---
+++ app/src/main/java/com/example/greetingapp/GreetingActivity.kt
@@ -0,0 +1,25 @@
+package com.example.greetingapp
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.greetingapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.goButton.setOnClickListener {
+            val name = binding.nameEditText.text.toString()
+            if (name.isNotEmpty()) {
+                val intent = Intent(this, GreetingActivity::class.java)
+                intent.putExtra("USER_NAME", name)
+                startActivity(intent)
+            }
+        }
+    }
+}
